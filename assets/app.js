a185805c648029fcecd191751d540c88bf98e540
Will <Will.irvin18@gmail.com>
2019-08-23 16:29:12 -0400

    got giphs to load onto screen!

--- assets/app.js
+++ assets/app.js
@@ -5,16 +5,15 @@ $(document).ready(function () {
   var animals = [];
   
   
-
   function createbutton() {
 
     $("#buttonbox").empty();
     for (var i = 0; i < animals.length; i++) {
       var a = $("<button id='newbutton'>")
       $("#buttonbox").prepend(a);
+      a.attr("data-name", animals[i])
       a.text(animals[i])
-
-    }
+       }
   };
 
 
@@ -22,22 +21,60 @@ $(document).ready(function () {
   $("#submit").on("click", function (event) {
 
     var input = $("#search").val().trim();
-    
-    
-
+    if (input === "") { }  else {
     animals.push(input)
-    
     createbutton();
     event.preventDefault();
     $("#search").val("");
 
-    
-
-
-    console.log(animals)
 
 
 
-  });
- 
+
+
+
+}});
+
+$("#buttonbox").on("click", "#newbutton", function() {
+  console.log("fuck")
+ $("#giphs").empty();
+var animal = $(this).attr("data-name")
+
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+animal + "&api_key=Ei0hI5Lo4ZSiZ5wWbYcuCqENZ5xH4REJ&limit=10";
+
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response){
+  var result = response.data;
+
+  for(var i = 0; i < result.length; i++) {
+  var giphBox = $("<div>")
+  var p = $("<p>").text("Rating: " + result[i].rating);
+  var animalImage = $("<img id='giphsize'>");
+  animalImage.attr("src", result[i].images.fixed_height.url);
+  giphBox.append(p);
+  giphBox.append(animalImage);
+  $("#giphs").prepend(giphBox);
+}
+  
+
+
+
+
+
+
+
 });
+});
+
+});
+
+
+// wire up buttons to call to API
+// set on click event for button to display gifs 
+// 
+// 
+//
