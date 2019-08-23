5b2505c7083e44421c2a30ff4036f93bd3676e3e
Will <Will.irvin18@gmail.com>
2019-08-23 14:57:26 -0400

    have boxes with text appearing

--- assets/app.js
+++ assets/app.js
@@ -1,26 +1,43 @@
-var animals = [];
+$(document).ready(function () {
 
 
- function createbutton() {
-    
-   
-   for(i = 0; i < animals.length; i++) {
-    var a = ("<button id='newbutton'>")
-    
-    $("#newbutton").text(animals[i])
-    $("#buttonbox").prepend(a);
-   }
- }
 
-
-$("#submit").on("click", function (event) {
-    var input = $("#search").val().trim();
-    $("#buttonbox").empty();
-    event.preventDefault();
-    
-   animals.push(input)
-   createbutton();
-   console.log(animals)
+  var animals = [];
   
-});
+  
 
+  function createbutton() {
+
+    $("#buttonbox").empty();
+    for (var i = 0; i < animals.length; i++) {
+      var a = $("<button id='newbutton'>")
+      $("#buttonbox").prepend(a);
+      a.text(animals[i])
+
+    }
+  };
+
+
+
+  $("#submit").on("click", function (event) {
+
+    var input = $("#search").val().trim();
+    
+    
+
+    animals.push(input)
+    
+    createbutton();
+    event.preventDefault();
+    $("#search").val("");
+
+    
+
+
+    console.log(animals)
+
+
+
+  });
+ 
+});
