0f4611910c1779c6b1c7ca19a68ee3d0966a6f13
Will <Will.irvin18@gmail.com>
2019-08-24 17:23:51 -0400

    put in start and stop gifs, made it look purty

--- assets/app.js
+++ assets/app.js
@@ -3,17 +3,17 @@ $(document).ready(function () {
 
 
   var animals = [];
-  
-  
+
+
   function createbutton() {
 
     $("#buttonbox").empty();
     for (var i = 0; i < animals.length; i++) {
-      var a = $("<button id='newbutton'>")
+      var a = $("<button id='newbutton' class='btn btn-danger'>")
       $("#buttonbox").prepend(a);
       a.attr("data-name", animals[i])
       a.text(animals[i])
-       }
+    }
   };
 
 
@@ -21,11 +21,11 @@ $(document).ready(function () {
   $("#submit").on("click", function (event) {
 
     var input = $("#search").val().trim();
-    if (input === "") { }  else {
-    animals.push(input)
-    createbutton();
-    event.preventDefault();
-    $("#search").val("");
+    if (input === "") { } else {
+      animals.push(input)
+      createbutton();
+      event.preventDefault();
+      $("#search").val("");
 
 
 
@@ -33,48 +33,64 @@ $(document).ready(function () {
 
 
 
-}});
+    }
+  });
 
-$("#buttonbox").on("click", "#newbutton", function() {
-  console.log("fuck")
- $("#giphs").empty();
-var animal = $(this).attr("data-name")
-
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-animal + "&api_key=Ei0hI5Lo4ZSiZ5wWbYcuCqENZ5xH4REJ&limit=10";
+  $("#buttonbox").on("click", "#newbutton", function () {
 
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response){
-  var result = response.data;
+    $("#giphs").empty();
+    var animal = $(this).attr("data-name")
 
-  for(var i = 0; i < result.length; i++) {
-  var giphBox = $("<div>")
-  var p = $("<p>").text("Rating: " + result[i].rating);
-  var animalImage = $("<img id='giphsize'>");
-  animalImage.attr("src", result[i].images.fixed_height.url);
-  giphBox.append(p);
-  giphBox.append(animalImage);
-  $("#giphs").prepend(giphBox);
-}
-  
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      animal + "&api_key=Ei0hI5Lo4ZSiZ5wWbYcuCqENZ5xH4REJ&limit=5";
 
 
 
 
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      var result = response.data;
+
+      for (var i = 0; i < result.length; i++) {
+        var giphBox = $("<div>")
+        var p = $("<p>").text("Rating: " + result[i].rating);
+        var animalImage = $("<img id='giphsize'>");
+        animalImage.attr("src", result[i].images.original_still.url);
+        animalImage.attr("data-state", "still");
+        animalImage.attr("data-still", result[i].images.original_still.url);
+        animalImage.attr("data-animate", result[i].images.fixed_height.url);
+
+        giphBox.append(p);
+        giphBox.append(animalImage);
+        $("#giphs").prepend(giphBox);
+
+        $("#giphsize").on("click", function () {
+
+          var state = $(this).attr("data-state");
+
+          if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+          } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+          }
+        });
+      }
 
 
+
+
+
+
+
+    });
+
+  });
 
 });
-});
-
-});
 
 
-// wire up buttons to call to API
-// set on click event for button to display gifs 
-// 
-// 
-//
